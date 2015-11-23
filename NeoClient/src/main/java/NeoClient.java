2df5cf7acbb7ac85475014219de69336d4efe50e
Colin Biafore <colinbiafore18@gmail.com>
2015-11-23 11:29:00 -0800

    Added createNode() and addProperty()

--- NeoClient/src/main/java/NeoClient.java
+++ NeoClient/src/main/java/NeoClient.java
@@ -1,10 +1,7 @@
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
-
 import java.util.*;
-
-
 import javax.ws.rs.core.MediaType;
 
 
@@ -15,21 +12,12 @@ public class NeoClient {
     public static void main(String[] args) {
 
 
-        /*
-        WebResource resource = Client.create().resource(SERVER_ROOT_URI);
-
-        ClientResponse response = resource.get(ClientResponse.class);
-        System.out.println( String.format("GET on [%s], status code [%d]",
-                SERVER_ROOT_URI, response.getStatus()));
-
-        response.close();
-        */
-
         Scanner sc = new Scanner(System.in);
+        String query = "";
 
         System.out.println("Welcome to the Neo4j client!");
         System.out.println("NOTE: Make sure to use single quotes when typing queries!");
-        String query = "";
+
 
         while(!query.equals("quit")) {
             System.out.print("> ");
@@ -41,6 +29,10 @@ public class NeoClient {
             }
 
         }
+
+
+        java.net.URI firstNode = createNode();
+        addProperty(firstNode, "name", "Colin Biafore");
 
     }
 
@@ -54,6 +46,7 @@ public class NeoClient {
         // Create data to send
         String payload = "{\"statements\" : [ {\"statement\" : \"" + query + "\"} ]}";
 
+        // POST
         ClientResponse response = resource
                 .accept( MediaType.APPLICATION_JSON )
                 .type( MediaType.APPLICATION_JSON)
@@ -63,14 +56,61 @@ public class NeoClient {
         System.out.println( String.format(
                 "POST [%s] to [%s], status code [%d], returned data: " + System.lineSeparator() + "%s",
                 payload, txUri, response.getStatus(),
-                response.getEntity( String.class )
-        ));
+                response.getEntity( String.class ) ));
 
         System.out.println();
 
         response.close();
 
 
+
+    }
+
+    public static java.net.URI createNode() {
+
+        final String nodeEntryPointUri = SERVER_ROOT_URI + "node/";
+
+        WebResource resource = Client.create()
+                .resource( nodeEntryPointUri );
+
+        // POST {} to the node entry point URI
+        ClientResponse response = resource
+                .accept( MediaType.APPLICATION_JSON )
+                .type( MediaType.APPLICATION_JSON )
+                .entity( "{}" )
+                .post( ClientResponse.class );
+
+        final java.net.URI location = response.getLocation();
+
+        System.out.println(String.format(
+                "POST to [%s], status code[%d], location header [%s]",
+                nodeEntryPointUri, response.getStatus(), location.toString() ));
+
+        response.close();
+
+        return location;
+
+    }
+
+    public static void addProperty(java.net.URI nodeUri, String propertyName, String propertyValue) {
+
+        String propertyUri = nodeUri.toString() + "/properties/" + propertyName;
+        // http://localhost:7474/db/data/node/{node_id}/properties/{property_name}
+
+        WebResource resource = Client.create()
+                .resource( propertyUri );
+
+        ClientResponse response = resource
+                .accept( MediaType.APPLICATION_JSON )
+                .type(MediaType.APPLICATION_JSON)
+                .entity( "\"" + propertyValue + "\"")
+                .put( ClientResponse.class );
+
+        System.out.println( String.format(
+                "PUT to [%s], status code [%d]",
+                propertyUri, response.getStatus() ));
+
+        response.close();
 
     }
 
